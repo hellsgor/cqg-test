fe725df39a74fd2f9d7607fc5f8966493be14c4b
Igor Storchevoy <hellsgor@gmail.com>
2023-12-05 21:05:46 +0400

    get-packages.service refactoring

--- src/app/modules/packages/services/get-packages.service.ts
+++ src/app/modules/packages/services/get-packages.service.ts
@@ -1,18 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable } from 'rxjs';
+import { BehaviorSubject, catchError, map, Observable, of } from 'rxjs';
 import { IPackage, IUrl } from '../models';
 
 @Injectable()
 export class GetPackagesService {
+  public packages$: BehaviorSubject<IPackage[]> = new BehaviorSubject<
+    IPackage[]
+  >([]);
+
   constructor(private readonly http: HttpClient) {}
 
   public getPackages({ protocol, host, port }: IUrl): Observable<IPackage[]> {
     return this.http
       .get<IPackage[]>(`${protocol}://${host}:${port}/packages`)
       .pipe(
+        map((packages: IPackage[]) => {
+          this.packages$.next(packages);
+          console.log(packages);
+          return packages;
+        }),
         catchError((error: unknown) => {
-          throw new Error('error in source. Details: ' + error);
+          console.error('error in source. Details: ' + error);
+          return of([]);
         })
       );
   }
